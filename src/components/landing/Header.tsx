febeeacbe65b941c7c1641d79471bb8c37e4b111
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2026-03-08 03:26:03 +0000

    minor: updated run iq current version

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -14,7 +14,7 @@ export function Header() {
           <span className="font-serif text-[22px] font-bold tracking-tight">
             RUN-IQ
           </span>
-          <Tag color="#6b7280">v0.1.0-alpha</Tag>
+          <Tag color="#6b7280">v0.2.0-stable</Tag>
         </div>
 
         {/* Mobile menu button */}
